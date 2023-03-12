d557418195b8bb176e83921b746f4f61f8d21cbe
ppotatoG <thgus2270@gmail.com>
2023-03-12 21:11:10 +0900

    Add: 22fe371

--- js/event.ts
+++ js/event.ts
@@ -1,5 +1,8 @@
-'use strict';
-const boxesData = [
+interface IBoxesData {
+  [key: string]: string;
+}
+
+const boxesData: IBoxesData[] = [
   { 'data-sa': 'fade' },
   { 'data-sa': 'from-right' },
   { 'data-sa': 'from-top' },
@@ -41,34 +44,44 @@ const boxesData = [
     'data-sa-delay': '500',
   },
 ];
+
 const createdBoxes = () => {
-  const boxWrap = document.querySelector('#box_wrap');
+  const boxWrap: Element | null = document.querySelector('#box_wrap');
+
   boxesData.forEach((boxData) => {
     const div = document.createElement('div');
     const pre = document.createElement('pre');
+
     for (const dataKey in boxData) {
       const dataValue = boxData[`${dataKey}`];
       div.classList.add('box');
       div.setAttribute(dataKey, dataValue);
       pre.innerHTML += `${dataKey}="${dataValue}"\n`;
     }
+
     div.appendChild(pre);
-    boxWrap === null || boxWrap === void 0 ? void 0 : boxWrap.appendChild(div);
+    boxWrap?.appendChild(div);
   });
 };
+
 (() => {
-  const mainText = document.querySelector('.mainText');
-  const mainTextArr = ['easy', 'simple'];
+  const mainText = document.querySelector('.mainText') as HTMLElement | null;
+  const mainTextArr: string[] = ['easy', 'simple'];
+
   const mainTextUl = document.createElement('ul');
-  mainText === null || mainText === void 0 ? void 0 : mainText.appendChild(mainTextUl);
-  mainTextArr.forEach((val) => {
+  mainText?.appendChild(mainTextUl);
+
+  mainTextArr.forEach((val: string) => {
     const li = document.createElement('li');
     li.innerText = `${val}`;
+
     mainTextUl.appendChild(li);
   });
-  const height = (mainText === null || mainText === void 0 ? void 0 : mainText.offsetHeight) || 0;
+
+  const height: number = mainText?.offsetHeight || 0;
   let flag = false;
   let i = 1;
+
   setInterval(() => {
     mainTextUl.style.transform = `translateY(-${height * i}px)`;
     if (flag) {
@@ -87,17 +100,21 @@ const createdBoxes = () => {
       i++;
     }
   }, 4000);
+
   createdBoxes();
 })();
-const header = document.querySelector('header');
-const sticky = (header === null || header === void 0 ? void 0 : header.offsetTop) || 0;
+
+const header = document.querySelector('header') as HTMLElement | null;
+const sticky: number = header?.offsetTop || 0;
+
 document.addEventListener('scroll', () => {
   if (window.screenY > sticky) {
-    header === null || header === void 0 ? void 0 : header.classList.add('fix');
+    header?.classList.add('fix');
   } else {
-    header === null || header === void 0 ? void 0 : header.classList.remove('fix');
+    header?.classList.remove('fix');
   }
 });
+
 window.addEventListener('DOMContentLoaded', () => {
-  sa.init(null);
+  sa.init(0.2);
 });
